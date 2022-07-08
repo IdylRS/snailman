53767bb3d861ce7d429758bfd9ac685bc761120e
IdylRS <will@idyl.live>
2022-07-08 16:18:19 -0600

    Add helm icon when alive

--- src/main/java/com/idyl/snailman/SnailManModePlugin.java
+++ src/main/java/com/idyl/snailman/SnailManModePlugin.java
@@ -10,26 +10,29 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.Point;
 import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.events.*;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
 import net.runelite.client.chat.ChatMessageManager;
 import net.runelite.client.chat.QueuedMessage;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.game.WorldService;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.JagexColors;
 import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.ColorUtil;
+import net.runelite.client.util.ImageUtil;
 import net.runelite.client.util.Text;
+import net.runelite.http.api.worlds.WorldResult;
+import net.runelite.http.api.worlds.World;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
@@ -65,11 +68,20 @@ public class SnailManModePlugin extends Plugin
 	@Inject
 	private ChatMessageManager chatMessageManager;
 
+	@Inject
+	private WorldService worldService;
+
+	@Inject
+	private ClientThread clientThread;
+
 	public Pathfinder pathfinder;
 	public Pathfinder.Path currentPath;
 
 	private int currentPathIndex;
-	private WorldPoint currentTarget;
+
+	private int snailmanIconOffset = -1;
+
+	private boolean onSeasonalWorld;
 
 	private WorldPoint snailWorldPoint;
 
@@ -86,6 +98,7 @@ public class SnailManModePlugin extends Plugin
 	private static final String TRANSPORT = ColorUtil.wrapWithColorTag("Transport", JagexColors.MENU_TARGET);
 	private static final String CONFIG_GROUP = "snailman";
 	private static final String CONFIG_KEY_SNAIL_LOC = "snailWorldPoint";
+	private static final String CONFIG_KEY_IS_ALIVE = "isAlive";
 	private static final WorldPoint DEFAULT_SNAIL_START = new WorldPoint(1181, 3624, 0);
 	public static final boolean DEV_MODE = false;
 
@@ -98,48 +111,10 @@ public class SnailManModePlugin extends Plugin
 	@Override
 	protected void startUp()
 	{
-		Map<SplitFlagMap.Position, byte[]> compressedRegions = new HashMap<>();
-		HashMap<WorldPoint, List<WorldPoint>> transports = new HashMap<>();
-
-		try (ZipInputStream in = new ZipInputStream(SnailManModePlugin.class.getResourceAsStream("/collision-map.zip"))) {
-			ZipEntry entry;
-			while ((entry = in.getNextEntry()) != null) {
-				String[] n = entry.getName().split("_");
-
-				compressedRegions.put(
-						new SplitFlagMap.Position(Integer.parseInt(n[0]), Integer.parseInt(n[1])),
-						Util.readAllBytes(in)
-				);
-			}
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		}
-
-		try {
-			String s = new String(Util.readAllBytes(SnailManModePlugin.class.getResourceAsStream("/transports.txt")), StandardCharsets.UTF_8);
-			Scanner scanner = new Scanner(s);
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-
-				if (line.startsWith("#") || line.isEmpty()) {
-					continue;
-				}
-
-				String[] l = line.split(" ");
-				WorldPoint a = new WorldPoint(Integer.parseInt(l[0]), Integer.parseInt(l[1]), Integer.parseInt(l[2]));
-				WorldPoint b = new WorldPoint(Integer.parseInt(l[3]), Integer.parseInt(l[4]), Integer.parseInt(l[5]));
-				transports.computeIfAbsent(a, k -> new ArrayList<>()).add(b);
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		CollisionMap map = new CollisionMap(64, compressedRegions);
-		pathfinder = new Pathfinder(map, transports);
-
+		loadResources();
 		isLoggedIn = false;
+		onSeasonalWorld = false;
 		isAlive = true;
-
 		overlayManager.add(snailManModeOverlay);
 		overlayManager.add(snailManModeMapOverlay);
 	}
@@ -149,6 +124,19 @@ public class SnailManModePlugin extends Plugin
 	{
 		overlayManager.remove(snailManModeOverlay);
 		overlayManager.remove(snailManModeMapOverlay);
+	}
+
+	private void addSnailmanIcon(ChatMessage chatMessage)
+	{
+		if(!isAlive) return;
+
+		String name = chatMessage.getName();
+
+		boolean isLocalPlayer = Text.standardize(name).equalsIgnoreCase(Text.standardize(client.getLocalPlayer().getName()));
+
+		if(!isLocalPlayer) return;
+
+		chatMessage.getMessageNode().setName(getImgTag(snailmanIconOffset)+Text.removeTags(name));
 	}
 
 	public WorldPoint getSnailWorldPoint() {
@@ -181,12 +169,127 @@ public class SnailManModePlugin extends Plugin
 		this.configManager.setRSProfileConfiguration(CONFIG_GROUP, CONFIG_KEY_SNAIL_LOC, snailWorldPoint);
 	}
 
+	private void saveData() {
+		if(this.configManager.getRSProfileKey() == null) return;
+
+		saveSnailWorldPoint();
+		configManager.setRSProfileConfiguration(CONFIG_GROUP, CONFIG_KEY_IS_ALIVE, isAlive);
+	}
+
 	@Subscribe
 	public void onMenuEntryAdded(MenuEntryAdded event) {
 		if (client.isKeyPressed(KeyCode.KC_SHIFT) && event.getOption().equals(WALK_HERE) && DEV_MODE) {
 			addMenuEntry(event, ADD_START);
 			addMenuEntry(event, ADD_END);
 		}
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if(gameStateChanged.getGameState() == GameState.LOGGED_IN && !isLoggedIn) {
+			final WorldPoint point = getSavedSnailWorldPoint();
+			String savedAlive = configManager.getRSProfileConfiguration(CONFIG_GROUP, CONFIG_KEY_IS_ALIVE);
+			setSnailWorldPoint(point);
+			currentPathIndex = 1;
+			isAlive = savedAlive == null ? true : Boolean.parseBoolean(savedAlive);
+			isLoggedIn = true;
+			onSeasonalWorld = isSeasonalWorld(client.getWorld());
+		}
+		else if(gameStateChanged.getGameState() == GameState.LOGIN_SCREEN && isLoggedIn){
+			isLoggedIn = false;
+			saveData();
+			currentPath = null;
+		}
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick tick) {
+		if(!isLoggedIn) return;
+
+		WorldPoint playerPoint = client.getLocalPlayer().getWorldLocation();
+
+		if(currentPath == null) {
+			currentPath = calculatePath(snailWorldPoint, playerPoint);
+		}
+
+		if(currentPathIndex < currentPath.getPath().size()) {
+			WorldPoint target = currentPath.getPath().get(currentPathIndex);
+			setSnailWorldPoint(target);
+			currentPathIndex++;
+		}
+
+		if(checkTouching()) {
+			final ChatMessageBuilder message = new ChatMessageBuilder()
+						.append(ChatColorType.HIGHLIGHT)
+						.append("You have been touched by the snail. You are dead.")
+						.append(ChatColorType.NORMAL);
+
+			if(isAlive) {
+				chatMessageManager.queue(QueuedMessage.builder()
+						.type(ChatMessageType.GAMEMESSAGE)
+						.runeLiteFormattedMessage(message.build())
+						.build());
+
+				client.playSoundEffect(SoundEffectID.ATTACK_HIT);
+
+				isAlive = false;
+				clientThread.invoke(() -> client.runScript(ScriptID.CHAT_PROMPT_INIT));
+				saveData();
+			}
+		}
+		moveSnailTowardsPlayer();
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage)
+	{
+		if (client.getGameState() != GameState.LOADING && client.getGameState() != GameState.LOGGED_IN)
+		{
+			return;
+		}
+
+		if(client.getLocalPlayer().getName() == null) return;
+
+		String name = Text.removeTags(chatMessage.getName());
+		boolean isSelf = client.getLocalPlayer().getName().equalsIgnoreCase(name);
+
+		switch (chatMessage.getType())
+		{
+			case PRIVATECHAT:
+			case MODPRIVATECHAT:
+				// Note this is unable to change icon on PMs if they are not a friend or in friends chat
+			case FRIENDSCHAT:
+				if (!onSeasonalWorld && isSelf)
+				{
+					addSnailmanIcon(chatMessage);
+				}
+				break;
+			case PUBLICCHAT:
+			case MODCHAT:
+				if (isSelf)
+				{
+					addSnailmanIcon(chatMessage);
+				}
+				break;
+		}
+	}
+
+	@Subscribe
+	public void onScriptCallbackEvent(ScriptCallbackEvent event)
+	{
+		if (!event.getEventName().equals("setChatboxInput"))
+		{
+			return;
+		}
+
+		updateChatbox();
+	}
+
+	@Subscribe
+	public void onBeforeRender(BeforeRender event)
+	{
+		updateChatbox();
 	}
 
 	private void addMenuEntry(MenuEntryAdded event, String option) {
@@ -222,56 +325,45 @@ public class SnailManModePlugin extends Plugin
 		}
 	}
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	private void updateChatbox()
 	{
-		if(gameStateChanged.getGameState() == GameState.LOGGED_IN && !isLoggedIn) {
-			final WorldPoint point = getSavedSnailWorldPoint();
-			setSnailWorldPoint(point);
-			currentPathIndex = 1;
-			isLoggedIn = true;
+		if(!isAlive) {
+			return;
 		}
-		else if(gameStateChanged.getGameState() == GameState.LOGIN_SCREEN && isLoggedIn){
-			isLoggedIn = false;
-			saveSnailWorldPoint();
-			currentPath = null;
+
+		Widget chatboxTypedText = client.getWidget(WidgetInfo.CHATBOX_INPUT);
+
+		if (snailmanIconOffset == -1)
+		{
+			return;
 		}
+
+		if (chatboxTypedText == null || chatboxTypedText.isHidden())
+		{
+			return;
+		}
+
+		String[] chatbox = chatboxTypedText.getText().split(":", 2);
+		String rsn = Objects.requireNonNull(client.getLocalPlayer()).getName();
+
+		chatboxTypedText.setText(getImgTag(snailmanIconOffset) + Text.removeTags(rsn) + ":" + chatbox[1]);
 	}
 
-	@Subscribe
-	public void onGameTick(GameTick tick) {
-		if(!isLoggedIn) return;
+	private static String getImgTag(int iconIndex)
+	{
+		return "<img=" + iconIndex + ">";
+	}
 
-		WorldPoint playerPoint = client.getLocalPlayer().getWorldLocation();
-		final int distanceFromPlayer = snailWorldPoint.distanceTo2D(playerPoint);
-
-		if(currentPath == null) {
-			currentPath = calculatePath(snailWorldPoint, playerPoint);
+	private boolean isSeasonalWorld(int worldNumber)
+	{
+		WorldResult worlds = worldService.getWorlds();
+		if (worlds == null)
+		{
+			return false;
 		}
 
-		if(currentPathIndex < currentPath.getPath().size()) {
-			WorldPoint target = currentPath.getPath().get(currentPathIndex);
-			setSnailWorldPoint(target);
-			currentPathIndex++;
-		}
-
-		if(checkTouching()) {
-			final ChatMessageBuilder message = new ChatMessageBuilder()
-						.append(ChatColorType.HIGHLIGHT)
-						.append("You have been touched by the snail. You are dead.")
-						.append(ChatColorType.NORMAL);
-
-			if(isAlive) {
-				chatMessageManager.queue(QueuedMessage.builder()
-						.type(ChatMessageType.GAMEMESSAGE)
-						.runeLiteFormattedMessage(message.build())
-						.build());
-
-				isAlive = false;
-			}
-		}
-
-		moveSnailTowardsPlayer();
+		World world = worlds.findWorld(worldNumber);
+		return world != null && world.getTypes().contains(WorldType.SEASONAL);
 	}
 
 	private void moveSnailTowardsPlayer() {
@@ -361,4 +453,62 @@ public class SnailManModePlugin extends Plugin
 		return null;
 	}
 
+	private void loadResources()
+	{
+		Map<SplitFlagMap.Position, byte[]> compressedRegions = new HashMap<>();
+		HashMap<WorldPoint, List<WorldPoint>> transports = new HashMap<>();
+
+		try (ZipInputStream in = new ZipInputStream(SnailManModePlugin.class.getResourceAsStream("/collision-map.zip"))) {
+			ZipEntry entry;
+			while ((entry = in.getNextEntry()) != null) {
+				String[] n = entry.getName().split("_");
+
+				compressedRegions.put(
+						new SplitFlagMap.Position(Integer.parseInt(n[0]), Integer.parseInt(n[1])),
+						Util.readAllBytes(in)
+				);
+			}
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+
+		try {
+			String s = new String(Util.readAllBytes(SnailManModePlugin.class.getResourceAsStream("/transports.txt")), StandardCharsets.UTF_8);
+			Scanner scanner = new Scanner(s);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+
+				if (line.startsWith("#") || line.isEmpty()) {
+					continue;
+				}
+
+				String[] l = line.split(" ");
+				WorldPoint a = new WorldPoint(Integer.parseInt(l[0]), Integer.parseInt(l[1]), Integer.parseInt(l[2]));
+				WorldPoint b = new WorldPoint(Integer.parseInt(l[3]), Integer.parseInt(l[4]), Integer.parseInt(l[5]));
+				transports.computeIfAbsent(a, k -> new ArrayList<>()).add(b);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		CollisionMap map = new CollisionMap(64, compressedRegions);
+		pathfinder = new Pathfinder(map, transports);
+
+		final IndexedSprite[] modIcons = client.getModIcons();
+
+		if (snailmanIconOffset != -1 || modIcons == null)
+		{
+			return;
+		}
+
+		BufferedImage image = ImageUtil.getResourceStreamFromClass(getClass(), "/helm.png");
+		IndexedSprite indexedSprite = ImageUtil.getImageIndexedSprite(image, client);
+
+		snailmanIconOffset = modIcons.length;
+
+		final IndexedSprite[] newModIcons = Arrays.copyOf(modIcons, modIcons.length + 1);
+		newModIcons[newModIcons.length - 1] = indexedSprite;
+
+		client.setModIcons(newModIcons);
+	}
 }
